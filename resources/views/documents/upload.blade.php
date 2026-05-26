10d937e946de6745d2e3c5132b0e0ea43fbd1453
Sinarta MJS PC <sinartamultijasasertifikasi@gmail.com>
2026-05-26 16:25:07 +0800

    update tombol download pakta integritas

--- resources/views/documents/upload.blade.php
+++ resources/views/documents/upload.blade.php
@@ -223,14 +223,20 @@
                 @foreach($templates->filter(function($template) use ($participant) {
                     return str_starts_with($template->type, $participant->type);
                 }) as $template)
-                    <a href="{{ route('template.download', $template) }}" class="download-btn">
+
+                    <a href="{{ url('storage/' . $template->file_path) }}" 
+                    class="download-btn" 
+                    target="_blank">
+
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                             <path d="M21 15v4a2 0 0 1-2 2H5a2 0 0 1-2-2v-4"></path>
                             <polyline points="7,10 12,15 17,10"></polyline>
                             <line x1="12" y1="15" x2="12" y2="3"></line>
                         </svg>
+
                         Download Template {{ $template->name }}
                     </a>
+
                 @endforeach
             </div>
             <div class="file-upload" onclick="triggerFileInput('integrity_pact')">
